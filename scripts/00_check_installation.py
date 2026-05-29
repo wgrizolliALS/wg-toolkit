0a777b23709e799f61cdb4f0ee26a0f72d04ba20
wgrizolliALS <walangrizolli@lbl.gov>
2026-05-29 13:44:32 -0700

    refactor: improve installation check script structure and error handling

--- scripts/00_check_installation.py
+++ scripts/00_check_installation.py
@@ -1,27 +1,36 @@
-# %% Check python installation and .venv environment (if in use)
+"""
+Simple script to verify the python and a few library installation and version.
+"""
+
+# %%
 import os
 import sys
 
 
 # %%
 
+print(f"\n## Python version: {sys.version}")
+print(f"## Python executable:\n\t- {sys.executable}")
+if ".venv" in sys.executable:
+    print("## WARNING: It looks like you are using a .venv environment.")
+
+# %%
+
+try:
+    import numpy as np
+
+    print("\n## numpy imported successfully")
+    print(f"## numpy version: {np.__version__}")
+except ImportError as e:
+    print(f"[ERROR] Error importing numpy: {e}")
+    print("[ERROR] Please ensure that numpy is installed and available in your Python environment.")
+    raise e
+
+# %%
+
 try:
     import wg_toolkit as wgtk
-    from wg_toolkit.logprint import (
-        printc,
-        print_log,
-        print_info,
-        print_warning,
-        print_attention,
-        print_error,
-        print_done,
-    )
-
-    print("\n")
-    print_info(f"## Python version: {sys.version}")
-    print_info(f"Python executable: {sys.executable}")
-    if ".venv" in sys.executable:
-        print_warning("It looks like you are using a .venv environment.")
+    from wg_toolkit.logprint import print_info, print_warning, print_done
 
     print("\n")
     print_done("wg_toolkit imported successfully")
@@ -30,3 +39,4 @@ try:
 except ImportError as e:
     print(f"[ERROR] Error importing wg_toolkit: {e}")
     print("[ERROR] Please ensure that wg_toolkit is installed and available in your Python environment.")
+    raise e
